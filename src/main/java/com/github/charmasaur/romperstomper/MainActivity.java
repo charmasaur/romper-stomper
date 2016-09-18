d489c66fc4af660de74a15609cdaf7d9e2783016
Harry Slatyer <harry.slatyer@gmail.com>
2016-09-18 20:38:18 +1000

    More general tidying.

--- src/main/java/com/github/charmasaur/romperstomper/MainActivity.java
+++ src/main/java/com/github/charmasaur/romperstomper/MainActivity.java
@@ -21,16 +21,10 @@ import java.util.List;
 
 public class MainActivity extends Activity {
   private static final String TAG = MainActivity.class.getSimpleName();
-  private static final String GOING = "Going";
-  private static final String NOT_GOING = "Not going";
   private static final int PERMISSION_CODE = 1337;
-
-  private boolean going;
 
   private TextView text;
   private TextView lastUpdatedText;
-  private ListView list;
-  private Switch button;
 
   private LocationRequester requester;
   private Sender sender;
@@ -48,25 +42,22 @@ public class MainActivity extends Activity {
 
     text = (TextView) findViewById(R.id.text);
     lastUpdatedText = (TextView) findViewById(R.id.last_updated);
-    button = (Switch) findViewById(R.id.button);
-    list = (ListView) findViewById(R.id.list);
-    list.setAdapter(adapter);
+    ((ListView) findViewById(R.id.list)).setAdapter(adapter);
 
     requester = new LocationRequester(this, requesterCallback);
     sender = new Sender(this, senderCallback);
 
-    button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-      @Override
-      public void onCheckedChanged(CompoundButton view, boolean isChecked) {
-        going = isChecked;
-        if (going) {
-          requester.go();
-        } else {
-          requester.stop();
-        }
-        updateButton();
-      }
-    });
+    ((Switch) findViewById(R.id.button)).setOnCheckedChangeListener(
+        new CompoundButton.OnCheckedChangeListener() {
+          @Override
+          public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+            if (isChecked) {
+              requester.go();
+            } else {
+              requester.stop();
+            }
+          }
+        });
 
     findViewById(R.id.update_button).setOnClickListener(new View.OnClickListener() {
       @Override
@@ -76,7 +67,6 @@ public class MainActivity extends Activity {
       }
     });
 
-    updateButton();
     getPermission();
   }
 
@@ -94,7 +84,7 @@ public class MainActivity extends Activity {
 
   private void getPermission() {
     String[] them = new String[] {Manifest.permission.ACCESS_FINE_LOCATION,
-      Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.INTERNET};
+        Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.INTERNET};
     if (!checkThePermissions(them)) {
       ActivityCompat.requestPermissions(this, them, PERMISSION_CODE);
     } else {
@@ -111,10 +101,6 @@ public class MainActivity extends Activity {
     return true;
   }
 
-  private void updateButton() {
-    text.setText(going ? GOING : NOT_GOING);
-  }
-
   private final Sender.Callback senderCallback = new Sender.Callback() {
     @Override
     public void onResults(List<String> results) {
@@ -122,6 +108,7 @@ public class MainActivity extends Activity {
       adapter.addAll(results);
       lastUpdatedText.setText("Last updated: " + DateFormat.getTimeInstance().format(new Date()));
     }
+
     @Override
     public void onStatus(String status) {
       text.setText(status);
