cb7423976a4dc6c017c73549ec649de7ab8debbc
Harry Slatyer <harry.slatyer@gmail.com>
2017-04-06 22:35:05 +1000

    Show polyline and marker labels, and status toasts

--- android/src/main/java/com/github/charmasaur/romperstomper/CycleMapActivity.java
+++ android/src/main/java/com/github/charmasaur/romperstomper/CycleMapActivity.java
@@ -7,13 +7,19 @@ import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.List;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
 
 public class CycleMapActivity extends FragmentActivity {
   private static final String TAG = CycleMapActivity.class.getSimpleName();
@@ -21,6 +27,8 @@ public class CycleMapActivity extends FragmentActivity {
   private CycleMapFetcher fetcher;
   @Nullable private GoogleMap googleMap;
   @Nullable private List<CycleMapFetcher.MarkerInfo> markers;
+
+  @Nullable private Toast toast;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -64,18 +72,46 @@ public class CycleMapActivity extends FragmentActivity {
 
   private void refresh() {
     fetcher.fetch();
+    toast("Fetching");
   }
 
   private void maybeUpdateMarkers() {
     if (googleMap == null || markers == null) {
       return;
     }
+    googleMap.clear();
+    LatLngBounds.Builder boundsBuilder = new LatLngBounds.Builder();
+    PolylineOptions options = new PolylineOptions();
+    LatLng prev = null;
     for (CycleMapFetcher.MarkerInfo marker : markers) {
+      LatLng latLng = new LatLng(marker.lat, marker.lng);
       googleMap.addMarker(
           new MarkerOptions()
-              .position(new LatLng(marker.lat, marker.lng))
-              .title("" + marker.timestamp));
+              .position(latLng)
+              .title(formatTimestamp(marker.timestamp)));
+      boundsBuilder.include(latLng);
+      if (prev != null) {
+        options.add(prev, latLng);
+      }
+      prev = latLng;
     }
+    googleMap.addPolyline(options);
+    googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(), 100));
+  }
+
+  private String formatTimestamp(long timestamp) {
+    SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss dd-MM-yyyy");
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTimeInMillis(timestamp * 1000);
+    return formatter.format(calendar.getTime());
+  }
+
+  private void toast(String message) {
+    if (toast != null) {
+      toast.cancel();
+    }
+    toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+    toast.show();
   }
 
   private final CycleMapFetcher.Callback fetcherCallback = new CycleMapFetcher.Callback() {
@@ -83,6 +119,11 @@ public class CycleMapActivity extends FragmentActivity {
     public void onSuccess(List<CycleMapFetcher.MarkerInfo> markers) {
       CycleMapActivity.this.markers = markers;
       maybeUpdateMarkers();
+      toast("Success");
+    }
+    @Override
+    public void onFailure() {
+      toast("Failure");
     }
   };
 
