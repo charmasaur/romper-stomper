465f80b6ebf138065f4de57a0743d1b34e39b44c
Harry Slatyer <harry.slatyer@gmail.com>
2021-01-24 17:21:51 +1100

    Finish migrating to flask

--- server/app/main.py
+++ server/app/main.py
@@ -1,16 +1,16 @@
 import datetime
+import json
 import os
 import time
 
-from flask import abort, request, render_template, url_for, make_response
+from flask import abort, request, render_template
 from flask_sqlalchemy import SQLAlchemy
-import base64
-import imghdr
 
 from app.app import app, db
 
 
 EXPIRY_TIME = datetime.timedelta(days=30)
+
 
 class CyclePath(db.Model):
     __tablename__ = "cycle_paths"
@@ -20,62 +20,76 @@ class CyclePath(db.Model):
     point_list = db.Column(db.String)
     expiry_date = db.Column(db.DateTime)
 
+
 def get_api_key():
     return os.getenv("MAPBOX_API_KEY", "")
 
 
 @app.route('/cycler', methods=['GET'])
 def cycler():
-    token = self.request.get('token', '')
+    token = request.args.get('token')
     if not token:
         self.response.write("You need to provide a token")
         return
-    native = self.request.get('native', '')
+    native = request.args.get('native')
 
-    path = ndb.Key(CyclePath, token).get()
+    path = CyclePath.query.filter_by(token=token).first()
+    if path:
+        point_list = json.loads(path.point_list)
+    else:
+        point_list = None
 
     if native:
-        if path == None:
+        if point_list == None:
             self.response.write("")
             return
-        self.response.write([[lat, lng, stamp] for (lat, lng, _, stamp) in path.point_list])
+        self.response.write([[lat, lng, stamp] for (lat, lng, _, stamp) in point_list])
         return
     else:
-        if path == None:
+        if point_list == None:
             self.response.write("Don't have any points yet, try again later")
             return
-        template = JINJA_ENVIRONMENT.get_template('cycle.html')
-        self.response.write(template.render(
-            {'list' : path.point_list, 'API_KEY' : get_api_key()}))
-        return
+        return render_template(
+            "cycle.html",
+            point_list=point_list,
+            API_KEY=get_api_key())
+
 
 @app.route("/cycle_submit", methods=['GET'])
 def cycle_submit():
-    token = self.request.get('token', '')
-    lat = float(self.request.get('lat', '0.0'))
-    lng = float(self.request.get('lng', '0.0'))
-    acc = float(self.request.get('acc', '0.0'))
+    token = request.args.get('token', '')
+    lat = float(request.args.get('lat', '0.0'))
+    lng = float(request.args.get('lng', '0.0'))
+    acc = float(request.args.get('acc', '0.0'))
     # tim is UTC seconds
-    tim = int(self.request.get('tim', '0'))
+    tim = int(request.args.get('tim', '0'))
 
     if lat == 0.0 and lng == 0.0:
         return
 
-    path = ndb.Key(CyclePath, token).get()
+    path = CyclePath.query.filter_by(token=token).first()
     if path == None:
-        path = CyclePath(id=token)
-        path.point_list = []
-        path.expiry_date = datetime.datetime.today() + EXPIRY_TIME
+        path = CyclePath(
+            token=token,
+            expiry_date=datetime.datetime.today() + EXPIRY_TIME,
+        )
+        point_list = []
+    else:
+        point_list = json.loads(path.point_list)
+
     # we assume the list is already sorted (in increasing order of time), so to keep it that
     # way we just need to look backwards through the list until we find something not bigger
     # than the new value (and then insert the new value after that element)
-    index = len(path.point_list) - 1
+    index = len(point_list) - 1
     while index >= 0:
-        if len(path.point_list[index]) < 4 or path.point_list[index][3] <= tim:
+        if len(point_list[index]) < 4 or point_list[index][3] <= tim:
             break
         index = index - 1
-    path.point_list.insert(index + 1, (lat, lng, time.ctime(tim + 11 * 60 * 60), tim))
-    path.put()
+    point_list.insert(index + 1, (lat, lng, time.ctime(tim + 11 * 60 * 60), tim))
+
+    path.point_list = json.dumps(point_list)
+    db.session.commit()
+    return "done"
 
 @app.route("/remove_expired", methods=['GET'])
 def remove_expired():
@@ -86,3 +100,7 @@ def remove_expired():
 
     ndb.delete_multi(query.iter(keys_only=True))
     self.response.write("Deleted " + str(query.count()) + " entries");
+
+@app.route("/dump", methods=['GET'])
+def dump():
+    return list(CyclePath.query())
