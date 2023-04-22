e74368cf161c1d3587935c12fdb2d62060200454
Harry Slatyer <harry.slatyer@gmail.com>
2023-04-22 19:24:09 +1000

    Add assetlinks

--- server/app/app.py
+++ server/app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 app.config.from_object("app.config.Config")
 db = SQLAlchemy(app)
