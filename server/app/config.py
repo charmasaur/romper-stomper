a3b228dac95fa44e49d2b4aee122db588e829889
Harry Slatyer <harry.slatyer@gmail.com>
2023-04-23 16:18:07 +1000

    Don't cache

--- server/app/config.py
+++ server/app/config.py
@@ -5,3 +5,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SEND_FILE_MAX_AGE_DEFAULT = 0
